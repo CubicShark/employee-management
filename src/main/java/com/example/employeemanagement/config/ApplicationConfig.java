d5352b990cdd15cc66964d3fd8cbcf7ebb7bd286
CubicShark <fathershark004@gmail.com>
2024-07-12 22:55:09 +0300

    add liquibase migrations

--- src/main/java/com/example/employeemanagement/config/ApplicationConfig.java
+++ src/main/java/com/example/employeemanagement/config/ApplicationConfig.java
@@ -1,8 +1,14 @@
 package com.example.employeemanagement.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
-    
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
